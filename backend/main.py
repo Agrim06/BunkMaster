977cfd56dc94708ae8fb6228f0993ba43d335911
Agrim06 <agrimkedia@gmail.com>
2026-01-23 18:14:52 +0530

    requirements.txt update

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # Allow all origins for Vercel/Render connection
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
