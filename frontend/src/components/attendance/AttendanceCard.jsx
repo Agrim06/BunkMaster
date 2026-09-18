9f9fc978828ae9b87b956070d9eb71c73eb3ab5f
Agrim06 <agrimkedia@gmail.com>
2026-09-18 17:39:18 +0530

    feat: add AttendanceCard component for subject attendance tracking

--- frontend/src/components/attendance/AttendanceCard.jsx
+++ frontend/src/components/attendance/AttendanceCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { markAttendance } from "../../api/attendance.api";
 import { resetSubject } from "../../api/subject.api";
 import CalendarView from "./CalendarView";
